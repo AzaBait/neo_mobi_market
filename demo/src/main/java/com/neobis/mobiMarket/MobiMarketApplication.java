c564932831590eb25d8c3b80259f4fcc33ed0a46
dell <m>
2023-12-22 11:34:19 +0600

    deploy

--- demo/src/main/java/com/neobis/mobiMarket/MobiMarketApplication.java
+++ demo/src/main/java/com/neobis/mobiMarket/MobiMarketApplication.java
@@ -1,9 +1,15 @@
 package com.neobis.mobiMarket;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.neobis.mobiMarket")
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL")})
 public class MobiMarketApplication {
 
 	public static void main(String[] args) {
