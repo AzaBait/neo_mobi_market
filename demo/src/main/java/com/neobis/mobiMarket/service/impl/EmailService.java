0ef6c8c449e7e38987cf546ef3b5dced925fc006
dell <m>
2024-01-12 22:15:44 +0600

    add setFrom

--- demo/src/main/java/com/neobis/mobiMarket/service/impl/EmailService.java
+++ demo/src/main/java/com/neobis/mobiMarket/service/impl/EmailService.java
@@ -6,6 +6,7 @@ import com.neobis.mobiMarket.repository.UserRepo;
 import com.neobis.mobiMarket.service.ActivationCodeService;
 import com.neobis.mobiMarket.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class EmailService {
+    @Value("")
+    private String from;
 
     private final JavaMailSender javaMailSender;
     private final UserRepo userRepo;
@@ -31,6 +34,7 @@ public class EmailService {
             activationCodeService.save(code);
         }
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("{gmail_username}");
         message.setTo(toEmail);
         message.setSubject("Activate your account");
         message.setText("Your activation code below:\n\n" + activationCode);
